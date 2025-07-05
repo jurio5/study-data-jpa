68981ae98bcd38925f134650882917206e833400
jurio5 <sia5492@naver.com>
2025-07-06 01:37:36 +0900

    [feat] p6spy 세팅

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -22,6 +22,7 @@ class MemberJpaRepositoryTest {
 
         Member findMember = memberJpaRepository.find(savedMember.getId());
 
+
         assertThat(findMember.getId()).isEqualTo(savedMember.getId());
         assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
         assertThat(findMember.getClass()).isEqualTo(savedMember.getClass());
