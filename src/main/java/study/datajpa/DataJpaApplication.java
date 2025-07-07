83561140a6d9bea4e11aa37ffa99ffbcd27b539c
jurio5 <sia5492@naver.com>
2025-07-08 02:13:17 +0900

    [feat] 스프링 부트를 사용하지 않을 때 환경설정과 Spring data jpa

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository") // 스프링 부트 사용 시 불필요
 public class DataJpaApplication {
 
     public static void main(String[] args) {
